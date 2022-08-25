13e0871fbd49ed39c1cf4cd96d507d3e3434074e
brainconnect93 <brainconnect0@gmail.com>
2022-08-25 11:10:12 +0100

    Complete transation query function

--- queries.sql
+++ queries.sql
@@ -19,7 +19,6 @@ ROLLBACK;
 SELECT * FROM animals;
 
 -- Update the animals table by setting the species column to digimon for all animals that have a name ending in mon.
-
 BEGIN;
 UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
 
@@ -39,6 +38,36 @@ SELECT * FROM animals;
 
 -- Inside a transaction:
     -- Delete all animals born after Jan 1st, 2022.
-
 BEGIN;
 DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+
+-- Create a savepoint for the transaction.
+SAVEPOINT savePoint1;
+
+-- Update all animals' weight to be their weight multiplied by -1
+UPDATE animals SET weight_kg = weight_kg * -1;
+
+-- Rollback to the savepoint
+ROLLBACK TO savePoint1;
+
+-- Update all animals' weights that are negative to be their weight multiplied by -1.
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+
+-- Commit transaction
+COMMIT;
+SELECT * FROM animals;
+
+
+-- Write queries to answer the following questions:
+    -- How many animals are there?
+
+
+
+
+
+How many animals are there?
+How many animals have never tried to escape?
+What is the average weight of animals?
+Who escapes the most, neutered or not neutered animals?
+What is the minimum and maximum weight of each type of animal?
+What is the average number of escape attempts per animal type of those born between 1990 and 2000?
