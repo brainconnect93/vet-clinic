ef8596526b847d5759ca21f31c132856d2d33afe
brainconnect93 <brainconnect0@gmail.com>
2022-08-30 08:28:32 +0100

    Add codes to the queries for data performance

--- queries.sql
+++ queries.sql
@@ -173,3 +173,7 @@ ORDER BY visits DESC;
 
 
 explain analyze SELECT COUNT(*) FROM visits where animals_id = 4;
+
+SELECT COUNT(*) FROM visits where animal_id = 4;
+SELECT * FROM visits where vet_id = 2;
+SELECT * FROM owners where email = 'owner_18327@mail.com';
